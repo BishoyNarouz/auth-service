a85cb0e8c693dc3b85608706bf2752ffe4149976
BishoyNarouz <bishoynarouz92@gmail.com>
2021-10-15 22:12:41 +0200

    console log

--- src/index.ts
+++ src/index.ts
@@ -13,6 +13,7 @@ const start = async () => {
       useUnifiedTopology: true,
       useCreateIndex: true
     });
+    console.log('scratch point of project')
     console.log('Connected to MongoDb');
   } catch (err) {
     console.error(err);
